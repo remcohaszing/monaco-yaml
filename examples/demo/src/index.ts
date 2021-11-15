90d895a21403d1adbb3fc956665465097274fb50
Remco Haszing <remcohaszing@gmail.com>
2021-11-15 18:30:47 +0100

    Flatten output paths
    `lib/esm/monaco.contribution.js` → `index.js`
    `lib/esm/yaml.worker.js` → `yaml.worker.js`
    
    Closes #132

--- examples/demo/src/index.ts
+++ examples/demo/src/index.ts
@@ -31,7 +31,7 @@ window.MonacoEnvironment = {
       case 'editorWorkerService':
         return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url));
       case 'yaml':
-        return new Worker(new URL('monaco-yaml/lib/esm/yaml.worker', import.meta.url));
+        return new Worker(new URL('monaco-yaml/yaml.worker', import.meta.url));
       default:
         throw new Error(`Unknown label ${label}`);
     }
