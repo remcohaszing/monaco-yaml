0518312cac4f6bb4a1d9cb8519df89bca9c8dfde
Dominik Seweryn <Dominik.Seweryn@canalplus.pl>
2021-11-24 18:12:44 +0100

    fix monaco-editor-webpack-plugin after library output change

--- examples/monaco-editor-webpack-plugin/index.js
+++ examples/monaco-editor-webpack-plugin/index.js
@@ -4,8 +4,29 @@ boolean: true
 `;
 
 async function create() {
-  const monaco = await import('./main.js');
+  // Dynamic import is possible
+  const { default: monaco } = await import('./main.js');
 
+  // Define schema first
+  monaco.languages.yaml.yamlDefaults.setDiagnosticsOptions({
+    schemas: [
+      {
+        fileMatch: ['*'],
+        uri: 'my-schema.json',
+        schema: {
+          type: 'object',
+          properties: {
+            number: {
+              description: 'number property',
+              type: 'number',
+            },
+          },
+        },
+      },
+    ],
+  });
+
+  // Create editor
   monaco.editor.create(document.querySelector('.editor'), {
     language: 'yaml',
     tabSize: 2,
