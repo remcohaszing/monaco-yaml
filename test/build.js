dc63938fd16991a2b12211ccce497da20d138d20
Remco Haszing <remcohaszing@gmail.com>
2022-09-20 16:22:12 +0200

    Fix error in test build script

--- test/build.js
+++ test/build.js
@@ -4,9 +4,9 @@ import { build } from 'esbuild';
 
 await build({
   entryPoints: {
-    'editor.worker.js': 'monaco-editor/esm/vs/editor/editor.worker.js',
-    'yaml.worker.js': fileURLToPath(new URL('../yaml.worker.js', import.meta.url)),
-    'index.js': fileURLToPath(new URL('index.ts', import.meta.url)),
+    'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
+    'yaml.worker': fileURLToPath(new URL('../yaml.worker.js', import.meta.url)),
+    index: fileURLToPath(new URL('index.ts', import.meta.url)),
   },
   bundle: true,
   format: 'iife',
