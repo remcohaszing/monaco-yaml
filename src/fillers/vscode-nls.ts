522c2dd4bf93e775e9fc4a653f18b90f1e7d545f
Remco Haszing <remcohaszing@gmail.com>
2023-05-27 21:21:25 +0200

    Remove unused types

--- src/fillers/vscode-nls.ts
+++ src/fillers/vscode-nls.ts
@@ -1,17 +1,9 @@
-export interface Options {
-  locale?: string
-  cacheLanguageResolution?: boolean
-}
-export interface LocalizeInfo {
+interface LocalizeInfo {
   key: string
   comment: string[]
 }
-export type LocalizeFunc = (
-  info: LocalizeInfo | string,
-  message: string,
-  ...args: string[]
-) => string
-export type LoadFunc = (file?: string) => LocalizeFunc
+type LocalizeFunc = (info: LocalizeInfo | string, message: string, ...args: string[]) => string
+type LoadFunc = (file?: string) => LocalizeFunc
 
 function format(message: string, args: string[]): string {
   return args.length === 0
