d3182feadf0b938bf4f4f5fbf58bcbd43175e967
Remco Haszing <remcohaszing@gmail.com>
2024-04-26 16:09:59 +0200

    Inline provider implementations

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
+import {
+  fromCodeActionContext,
+  fromPosition,
+  fromRange,
+  toCodeAction,
+  toCompletionList,
+  toDocumentSymbol,
+  toFoldingRange,
+  toHover,
+  toLink,
+  toLocationLink,
+  toMarkerData,
+  toTextEdit
+} from 'monaco-languageserver-types'
 import { registerMarkerDataProvider } from 'monaco-marker-data-provider'
 import { type MonacoEditor } from 'monaco-types'
 import { createWorkerManager } from 'monaco-worker-manager'
 import { type MonacoYaml, type MonacoYamlOptions } from 'monaco-yaml'
 
-import {
-  createCodeActionProvider,
-  createCompletionItemProvider,
-  createDefinitionProvider,
-  createDocumentFormattingEditProvider,
-  createDocumentSymbolProvider,
-  createFoldingRangeProvider,
-  createHoverProvider,
-  createLinkProvider,
-  createMarkerDataProvider
-} from './languageFeatures.js'
 import { type YAMLWorker } from './yaml.worker.js'
 
 export function configureMonacoYaml(monaco: MonacoEditor, options: MonacoYamlOptions): MonacoYaml {
@@ -37,47 +40,136 @@ export function configureMonacoYaml(monaco: MonacoEditor, options: MonacoYamlOpt
     mimetypes: ['application/x-yaml']
   })
 
-  const worker = createWorkerManager<YAMLWorker, MonacoYamlOptions>(monaco, {
+  const workerManager = createWorkerManager<YAMLWorker, MonacoYamlOptions>(monaco, {
     label: 'yaml',
     moduleId: 'monaco-yaml/yaml.worker',
     createData
   })
 
-  let markerDataProvider = registerMarkerDataProvider(
-    monaco,
-    'yaml',
-    createMarkerDataProvider(worker.getWorker)
-  )
+  const markerDataProvider = registerMarkerDataProvider(monaco, 'yaml', {
+    owner: 'yaml',
+
+    async provideMarkerData(model) {
+      const worker = await workerManager.getWorker(model.uri)
+      const diagnostics = await worker.doValidation(String(model.uri))
+
+      return diagnostics?.map((diagnostic) => toMarkerData(diagnostic))
+    },
+
+    async doReset(model) {
+      const worker = await workerManager.getWorker(model.uri)
+      await worker.resetSchema(String(model.uri))
+    }
+  })
+
   const disposables = [
-    worker,
+    workerManager,
+    markerDataProvider,
 
-    monaco.languages.registerCompletionItemProvider(
-      'yaml',
-      createCompletionItemProvider(worker.getWorker)
-    ),
+    monaco.languages.registerCompletionItemProvider('yaml', {
+      triggerCharacters: [' ', ':'],
 
-    monaco.languages.registerHoverProvider('yaml', createHoverProvider(worker.getWorker)),
+      async provideCompletionItems(model, position) {
+        const wordInfo = model.getWordUntilPosition(position)
+        const worker = await workerManager.getWorker(model.uri)
+        const info = await worker.doComplete(String(model.uri), fromPosition(position))
 
-    monaco.languages.registerDefinitionProvider('yaml', createDefinitionProvider(worker.getWorker)),
+        if (info) {
+          return toCompletionList(info, {
+            range: {
+              startLineNumber: position.lineNumber,
+              startColumn: wordInfo.startColumn,
+              endLineNumber: position.lineNumber,
+              endColumn: wordInfo.endColumn
+            }
+          })
+        }
+      }
+    }),
 
-    monaco.languages.registerDocumentSymbolProvider(
-      'yaml',
-      createDocumentSymbolProvider(worker.getWorker)
-    ),
+    monaco.languages.registerHoverProvider('yaml', {
+      async provideHover(model, position) {
+        const worker = await workerManager.getWorker(model.uri)
+        const info = await worker.doHover(String(model.uri), fromPosition(position))
 
-    monaco.languages.registerDocumentFormattingEditProvider(
-      'yaml',
-      createDocumentFormattingEditProvider(worker.getWorker)
-    ),
+        if (info) {
+          return toHover(info)
+        }
+      }
+    }),
 
-    monaco.languages.registerLinkProvider('yaml', createLinkProvider(worker.getWorker)),
+    monaco.languages.registerDefinitionProvider('yaml', {
+      async provideDefinition(model, position) {
+        const worker = await workerManager.getWorker(model.uri)
+        const locationLinks = await worker.doDefinition(String(model.uri), fromPosition(position))
 
-    monaco.languages.registerCodeActionProvider('yaml', createCodeActionProvider(worker.getWorker)),
+        return locationLinks?.map(toLocationLink)
+      }
+    }),
 
-    monaco.languages.registerFoldingRangeProvider(
-      'yaml',
-      createFoldingRangeProvider(worker.getWorker)
-    ),
+    monaco.languages.registerDocumentSymbolProvider('yaml', {
+      displayName: 'yaml',
+
+      async provideDocumentSymbols(model) {
+        const worker = await workerManager.getWorker(model.uri)
+        const items = await worker.findDocumentSymbols(String(model.uri))
+
+        return items?.map(toDocumentSymbol)
+      }
+    }),
+
+    monaco.languages.registerDocumentFormattingEditProvider('yaml', {
+      displayName: 'yaml',
+
+      async provideDocumentFormattingEdits(model) {
+        const worker = await workerManager.getWorker(model.uri)
+        const edits = await worker.format(String(model.uri))
+
+        return edits?.map(toTextEdit)
+      }
+    }),
+
+    monaco.languages.registerLinkProvider('yaml', {
+      async provideLinks(model) {
+        const worker = await workerManager.getWorker(model.uri)
+        const links = await worker.findLinks(String(model.uri))
+
+        if (links) {
+          return {
+            links: links.map(toLink)
+          }
+        }
+      }
+    }),
+
+    monaco.languages.registerCodeActionProvider('yaml', {
+      async provideCodeActions(model, range, context) {
+        const worker = await workerManager.getWorker(model.uri)
+        const codeActions = await worker.getCodeAction(
+          String(model.uri),
+          fromRange(range),
+          fromCodeActionContext(context)
+        )
+
+        if (codeActions) {
+          return {
+            actions: codeActions.map((codeAction) => toCodeAction(codeAction)),
+            dispose() {
+              // This is required by the TypeScript interface, but it’s not implemented.
+            }
+          }
+        }
+      }
+    }),
+
+    monaco.languages.registerFoldingRangeProvider('yaml', {
+      async provideFoldingRanges(model) {
+        const worker = await workerManager.getWorker(model.uri)
+        const foldingRanges = await worker.getFoldingRanges(String(model.uri))
+
+        return foldingRanges?.map(toFoldingRange)
+      }
+    }),
 
     monaco.languages.setLanguageConfiguration('yaml', {
       comments: {
@@ -117,11 +209,10 @@ export function configureMonacoYaml(monaco: MonacoEditor, options: MonacoYamlOpt
       for (const disposable of disposables) {
         disposable.dispose()
       }
-      markerDataProvider.dispose()
     },
 
     async update(newOptions) {
-      worker.updateCreateData(Object.assign(createData, newOptions))
+      workerManager.updateCreateData(Object.assign(createData, newOptions))
       await markerDataProvider.revalidate()
     }
   }
