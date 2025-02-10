4e4de9206288ed988a31d6b35ade3f0b3c38e5f3
Remco Haszing <remcohaszing@gmail.com>
2025-02-10 16:38:29 +0100

    Specify which deps Vitest should optimize

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  optimizeDeps: {
+    include: [
+      'jsonc-parser',
+      'monaco-worker-manager/worker',
+      'path-browserify',
+      'prettier/plugins/yaml',
+      'prettier/standalone',
+      'vscode-languageserver-textdocument',
+      'vscode-languageserver-types',
+      'vscode-uri',
+      'yaml'
+    ]
+  },
   test: {
     setupFiles: ['test/setup.ts'],
     browser: {
