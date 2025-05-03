7ff360405c7beb62476acdc3c7658e55ebf3a629
Remco Haszing <remcohaszing@gmail.com>
2025-05-03 20:38:16 +0200

    Update to yaml-language-server 1.18.0

--- fillers/ajv.ts
+++ fillers/ajv.ts
@@ -1,11 +1,6 @@
 export default class AJVStub {
   // eslint-disable-next-line @typescript-eslint/class-methods-use-this
-  defaultMeta(): undefined {
-    // This is a stub
-  }
-
-  // eslint-disable-next-line @typescript-eslint/class-methods-use-this
-  validateSchema(): boolean {
-    return true
+  compile(): () => boolean {
+    return () => true
   }
 }
